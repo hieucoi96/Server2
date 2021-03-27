1bc544a3ca0ec5096d463a938c9632786cf2a602
hieucoi669 <62107676+hieucoi669@users.noreply.github.com>
2021-03-27 12:56:45 +0700

    Update

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var http = require('http');
 
 http.createServer(function ( reg, res){
-    res.end('Xin chao mn nhe!!');
+    // res.end('Xin chao mn nhe!!');
+    if (url == '/') {
+        response.writeHead('200', {'Content-Type': 'text/html'});
+        fs.readFile('index.html', function (error, data) {
+            if (error == null) {
+                response.write(data);
+                response.end();
+            } else {
+                response.end(error);
+            }
+        });
+    } else response.end('404 Not Found');
 }).listen(process.env.PORT || 3000);
