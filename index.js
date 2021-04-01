56e3caf6201596a194dd22c8f0ad40edecde1148
hieucoi669 <62107676+hieucoi669@users.noreply.github.com>
2021-04-01 18:27:20 +0700

    Update

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var path = require('path');
+var expressHDS = require('express-handlebars')
+var app = express()
+app.use(express.static(path.join(__dirname, '/assets')));
+app.engine("handlebars", expressHDS({
+    layoutsDir: __dirname + '/views/layouts',
+    defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
+app.get('/', function (request, response){
+    response.render('home');
+});
 
-http.createServer(function ( request, response){
-    // res.end('Xin chao mn nhe!!');
-    var url = request.url;
-    if (url == '/') {
-        response.writeHead('200', {'Content-Type': 'text/html'});
-        fs.readFile('index.html', function (error, data) {
-            if (error == null) {
-                response.write(data);
-                response.end();
-            } else {
-                response.end(error);
-            }
-        });
-    } else response.end('404 Not Found');
-}).listen(process.env.PORT || 3000);
+// app.post('/login', function (request, response){
+//
+// });
+
+app.listen((process.env.PORT || '3001'))
